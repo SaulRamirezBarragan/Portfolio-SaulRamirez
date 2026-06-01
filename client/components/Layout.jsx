ba7ce23325fd6b9e905d00835f36cb8600dc21cf
SaulRamirezBarragan <saulramirezb@hotmail.com>
2026-06-01 00:15:28 -0400

    Education page

--- client/components/Layout.jsx
+++ client/components/Layout.jsx
@@ -6,8 +6,8 @@ export default function Layout() {
             <h1>My Portfolio</h1>
             <nav>
                 <Link to="/">Home</Link> | <Link to="/about">About Me</Link> |
-<Link to="/education">Education</Link>| <Link to="/project">Project</Link>| 
-<Link to="/contact">Contact</Link> | <Link to="/counter">Counter</Link>
+<Link to="/education"> Education</Link> | <Link to="/project">Project</Link> | 
+<Link to="/contact"> Contact</Link> | <Link to="/counter">Counter</Link>
             </nav>
             <hr />
         </div>
